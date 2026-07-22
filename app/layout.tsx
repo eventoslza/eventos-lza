20401faeda0f7c3382e6b21f2859599ce40a059b
eventoslza <eventosluziania.regiao02@gmail.com>
2026-07-22 15:46:53 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
-import './globals.css'
-export const metadata = {
-  title: 'Eventos LZA - O Portal da Região',
-  description: 'Divulgação e promoção de eventos em Luziânia e região.',
-}
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body style={{ backgroundColor: '#050505', color: 'white', margin: 0 }}>{children}</body>
+      <body style={{ margin: 0, backgroundColor: '#050505' }}>{children}</body>
     </html>
   )
 }
