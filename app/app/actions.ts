3186de3b3d77d84a079f1e4f1436e359dea25b4f
eventoslza <eventosluziania.regiao02@gmail.com>
2026-07-22 17:50:37 -0300

    Create actions.ts

---
+++ app/app/actions.ts
@@ -0,0 +1,31 @@
+"use server"
+import { PrismaClient } from '@prisma/client'
+import { revalidatePath } from 'next/cache'
+
+const prisma = new PrismaClient()
+
+export async function criarEvento(formData: FormData) {
+  const nome = formData.get("nome") as string
+  const data = formData.get("data") as string
+  const cidade = formData.get("cidade") as string
+  const imagemUrl = formData.get("imagemUrl") as string
+  const link = formData.get("link") as string
+
+  await prisma.evento.create({
+    data: { 
+      nome, 
+      data: new Date(data), 
+      cidade, 
+      imagemUrl, 
+      link,
+      ativo: true 
+    }
+  })
+
+  revalidatePath('/')
+}
+
+export async function deletarEvento(id: string) {
+  await prisma.evento.delete({ where: { id } })
+  revalidatePath('/')
+}
